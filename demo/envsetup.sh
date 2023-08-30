5a7de30372905e578531bc27728e15428ac8751c
pramod870 <tomarpramod0532@gmail.com>
2023-08-30 10:29:35 +0530

    CI CD jenkins

---
+++ demo/envsetup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [-d "envdev"]
+then
+echo "python virtual env exists"
+else
+    python3 -m venv env
+fi
+echo $pwd
+source env/bin/activate
+pip3 install -r requirements.txt    
+
+
+if [-d "logs"]
+then
+echo "log folder exists"
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
+sudo chmod -R 777 logs
+echo "envsetup finishes"  
